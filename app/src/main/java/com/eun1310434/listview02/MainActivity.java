9dd9575773c3080efd216d84b06938580c6ab45f
eun1310434 <eun1310434@naver.com>
2018-05-23 11:07:55 +0200

    -

--- app/src/main/java/com/eun1310434/listview02/MainActivity.java
+++ app/src/main/java/com/eun1310434/listview02/MainActivity.java
@@ -1,17 +1,24 @@
 /*=====================================================================
-□ Infomation
-  ○ Data : 08.03.2018
+□ INFORMATION
+  ○ Data : 23.05.2018
   ○ Mail : eun1310434@naver.com
   ○ Blog : https://blog.naver.com/eun1310434
-  ○ Reference : Do it android app Programming
+  ○ Reference
+     - Do it android app Programming
+     - Hello JAVA Programming
+     - http://itmining.tistory.com/5
 
-□ Function
+□ FUNCTION
   ○ 간단한 리스트 뷰 만들기
   ○ ListActivity를 상속받아 구현
      - actionBar가 안보임
 
-□ Study
-  ○
+□ STUDY
+  ○ ListActivity
+     -An activity that displays a list of items by binding to a data source such as an array or Cursor,
+      and exposes event handlers when the user selects an item.
+     -ListActivity hosts a ListView object that can be bound to different data sources,
+      typically either an array or a Cursor holding query results. Binding, screen layout, and row layout are discussed in the following sections.
 =====================================================================*/
 package com.eun1310434.listview02;
 
